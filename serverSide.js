591f1c8c0553e582bb869ffba52bf52a1ccddebe
Fabien JUIF <fabien.juif@zenika.com>
2016-06-15 21:08:08 +0200

    First PDF version

--- serverSide.js
+++ serverSide.js
@@ -1,14 +1,1 @@
-const React = require('react')
-const cleanupDom = require('jsdom-global')('<html><div id="app"/></html>')
-const Component = require('./build/index.js').default
-
-const render = require('react-dom').render
-
-render(
-  React.createElement(Component, null)
-  , document.getElementById('app')
-)
-
-console.log(document.documentElement.innerHTML)
-
-cleanupDom()
+console.log(require('./reactToString'))
